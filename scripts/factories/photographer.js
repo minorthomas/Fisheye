0d6dc122bab11bdcb4fe7fdce8712568064d0248
M. Thomas <mthomas.pro@outlook.fr>
2022-05-11 12:16:56 +0200

    style index.html

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,12 +6,16 @@ function photographerFactory(data) {
         const article = document.createElement('article');
 
         const photographersCard = `
-            <a href="photographer.html?id=${data.id}">
-                <img src="${picture}" alt="Profil de ${data.name}"/>
-                <h2>${data.name}</h2>
-                <h3>${data.city}, ${data.country}</h3>
-                <p>${data.tagline}</p>
-                <p>${data.price}€/jour</p>
+            <a class="photographer_link" href="photographer.html?id=${data.id}">
+                <div class="photographer_header">
+                    <img src="${picture}" alt="Profil de ${data.name}"/>
+                    <h2>${data.name}</h2>
+                </div>
+                <div class="photographer_footer">
+                    <h3>${data.city}, ${data.country}</h3>
+                    <p class="photographer_tagline">${data.tagline}</p>
+                    <p class="photographer_price">${data.price}€/jour</p>
+                </div>
             </a>
         `
         article.innerHTML = photographersCard;
