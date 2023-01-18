590f72ad8c968ad6698884607ad7c802e0c283a5
MINOR Thomas <mthomas.pro@outlook.fr>
2023-01-18 15:16:05 +0100

    v0.6.5
    - modal code refactoring
    - add new class for modal
    - remove resize for textarea
    - remove pointer events when modal is open

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -4,45 +4,51 @@ const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 
 function displayModal() {
-    modal.style.display = "flex";
-    main.style.opacity = "0.4";
-    header.style.opacity = "0.4";
-    footer.style.opacity = "0.4";
+    header.classList.add("opacity_medium");
+    main.classList.add("opacity_medium");
+    footer.classList.add("opacity_medium");
+    modal.classList.add("display_flex");
 }
 
 function closeModal() {
-    modal.style.display = "none";
-    main.style.opacity = "1";
-    header.style.opacity = "1";
-    footer.style.opacity = "1";
+    header.classList.remove("opacity_medium");
+    main.classList.remove("opacity_medium");
+    footer.classList.remove("opacity_medium");
+    modal.classList.remove("display_flex");
 }
 
 async function displayNameModal() {
-    const { photographers } = await getPhotographers(); //recup photographers et medias
+    const { photographers } = await getPhotographers();
 
-    const selectedPhotographer = photographers.find( //trouve et verifi si id du photographer == id dans l'url
+    const selectedPhotographer = photographers.find(
         (photographer) => photographer.id == getIdParam
     );
 
     const selectedPhotographerName = new Photographer(selectedPhotographer);
 
-    const headerSelectedPhotographerName = document.querySelector("#modal_header div p");
+    const headerSelectedPhotographerName = document.querySelector(
+        "#modal_header div p"
+    );
     headerSelectedPhotographerName.innerHTML = selectedPhotographerName._name;
 }
 
 function displayValuesInLog() {
     const sendFormButton = document.querySelector("#send_form_button");
 
-    const firstname = document.querySelector('#firstname');
-    const lastname = document.querySelector('#lastname');
-    const email = document.querySelector('#email');
-    const message = document.querySelector('#yourmessage');
-
+    const firstname = document.querySelector("#firstname");
+    const lastname = document.querySelector("#lastname");
+    const email = document.querySelector("#email");
+    const message = document.querySelector("#yourmessage");
 
     sendFormButton.addEventListener("click", (event) => {
         event.preventDefault();
 
-        if (firstname.value.length == 0 || lastname.value.length == 0 || email.value.length == 0 || message.value.length == 0) {
+        if (
+            firstname.value.length == 0 ||
+            lastname.value.length == 0 ||
+            email.value.length == 0 ||
+            message.value.length == 0
+        ) {
             console.log("Error, vous devez remplir tous les champs");
         } else {
             console.clear();
